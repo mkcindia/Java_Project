e2de642fd63a824bd80529a192018b19bfde7fc6
Dharmik <mkcindiaofficial@gmail.com>
2024-06-19 22:46:50 +0530

    code_Update
    code_Update

--- database/Database.java
+++ database/Database.java
@@ -1,39 +1,3 @@
-// package database;
-
-// import models.User;
-// import models.Product;
-// import java.util.HashMap;
-// import java.util.Map;
-
-// public class Database {
-//     private Map<String, User> userMap;
-//     private Map<String, Product> productMap;
-
-//     public Database() {
-//         this.userMap = new HashMap<>();
-//         this.productMap = new HashMap<>();
-//     }
-
-//     public void addUser(User user) {
-//         userMap.put(user.getId(), user);
-//     }
-
-//     public User getUser(String id) {
-//         return userMap.get(id);
-//     }
-
-//     public void removeUser(String id) {
-//         userMap.remove(id);
-//     }
-
-//     public void addProduct(Product product) {
-//         productMap.put(product.getId(), product);
-//     }
-
-//     public void removeProduct(String productId) {
-//         productMap.remove(productId);
-//     }
-// }
 package database;
 
 import models.User;
